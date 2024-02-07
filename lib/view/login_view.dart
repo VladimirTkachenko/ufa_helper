ca7238852f2da4930873657d908157ba5a5caf3c
Vladimir_Tkachenko <tkachenko.vladimir.kh@gmail.com>
2024-02-07 13:50:33 +0000

    init

---
+++ lib/view/login_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({Key? super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('LoginView'),
+    );
+  }
+}
